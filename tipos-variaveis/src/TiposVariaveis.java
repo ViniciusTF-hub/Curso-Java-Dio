069963717fca7b944ac8fef73b70e43d545eee68
ViniciusTF-hub <vinitavaresvini27@gmail.com>
2024-07-29 15:58:09 -0300

    Adicionei variaveis do tipo inteiro, fracionado e do tipo texto

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+       final String BR="Brasil";
+       int idade=18;
+       double salarioMedio=1500;
+
     }
 }
