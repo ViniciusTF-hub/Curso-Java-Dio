b3b9248630386625f8bde89d9b77c92994ab2866
ViniciusTF-hub <vinitavaresvini27@gmail.com>
2024-07-29 16:35:39 -0300

    Adicionado outras variaveis com operadores lógicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,25 @@
 public class TiposVariaveis {
 public static void main(String[] args) throws Exception {
+        
         System.out.println("Hello, World!");
-       final String BR="Brasil";
-       int idade=18;
-       double salarioMedio=1500;
+            final String BR="Brasil";
+            int idade=18;
+            double salarioMedio=1500;
+            int somar= 10+20;
+            double subtrair= 500.10-115.5;
+            int multiplicar= 40*50;
+            int dividir= 100/2;
+            int modulo= 18%3;
+            int resultado= (40*6) + (20-7);
+                    System.err.println(somar);
+                    System.err.println(subtrair);
+                    System.err.println(multiplicar);
+                    System.err.println(dividir);
+                    System.err.println(modulo);
+                    System.err.println(resultado);
+                    System.err.println(BR);
+                    System.err.println(idade);
+                    System.err.println(salarioMedio);
 
     }
 }
