43e5157bdd0f14136f9ea49d15212e006087b018
ViniciusTF-hub <vinitavaresvini27@gmail.com>
2024-07-29 17:30:41 -0300

    Adicionei operador tenario  e unario

--- tipos-variaveis/src/OperadoresUnarios.java
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -4,6 +4,11 @@ public class OperadoresUnarios {
        numero=-5;
        System.out.println(numero);
        System.out.println(numero * -1);
+        int a,b;
+        a=10;
+        b=100;
+        String resultado= a==b ? "verdadeiro": "falso";
+        System.out.print(resultado);
     }
     }
  
