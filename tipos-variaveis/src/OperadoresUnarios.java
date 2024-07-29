b06bc9ced56e2cf0efc39921e1f84fa67f664bce
ViniciusTF-hub <vinitavaresvini27@gmail.com>
2024-07-29 17:11:03 -0300

    adicionei operadores unarios positivo e negativo

---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,11 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+       int numero= 5;
+       numero=-5;
+       System.out.println(numero);
+       System.out.println(numero * -1);
+    }
+    }
+ 
+ 
+
